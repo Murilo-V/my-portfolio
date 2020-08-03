34a223d10bc4608cef74bd1660dcd3739ff185f8
Alveira <alveiramurilo@hotmail.com>
2020-08-03 15:43:39 -0300

    first commit

---
+++ scripts/header.js
@@ -0,0 +1,22 @@
+function changeHeader() {
+    const header = document.getElementById('menu'); // colocar em cache
+    
+    window.addEventListener('scroll', function (){
+        if (window.scrollY > 0) {
+            header.style.backgroundColor = "#D5DBD5";
+            header.style.transition = "300ms"
+            header.style.transitionProperty = "background-color"
+        }
+        else {
+            header.style.backgroundColor = ""
+        }
+    })
+};
+
+changeHeader();
+
+// $(document).ready(function(){
+// 	$("#welcome").scroll(function(){
+// 		$("#menu").css("background","#D5DBD5");
+// 	});
+// });
