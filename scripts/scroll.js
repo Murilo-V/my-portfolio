42e6dcebb0d0e32ee5e5fd05e4dbbc9450100fbe
Alveira <alveiramurilo@hotmail.com>
2020-08-07 11:16:59 -0300

    fix: resolved bug smooth scroll

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,14 +1,6 @@
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        targetOffset = $(hash).offset().top
+function homePage(){
+        targetOffset = $("#home-page").offset().top
         $('html, body').animate({
           scrollTop: targetOffset - 90
-        }, 700, function(){
-          window.location.hash = hash;
-        });
-      } 
-    });
-  });
+        }, 700);
+}
