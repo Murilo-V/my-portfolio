01eccac888c4dbc1ef9819d1462916a5861de0c4
Alveira <alveiramurilo@hotmail.com>
2020-08-08 14:48:19 -0300

    feat: added hard skills

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,6 +1,13 @@
-function homePage(){
-        targetOffset = $("#home-page").offset().top
+function welcomeScroll(){
+        targetOffset = $("#presentation").offset().top
         $('html, body').animate({
-          scrollTop: targetOffset - 90
+          scrollTop: targetOffset - 80
         }, 700);
+}
+
+function skillsScroll(){
+  targetOffset = $("#hard-skills").offset().top
+  $('html, body').animate({
+    scrollTop: targetOffset - 80
+  }, 700);
 }
