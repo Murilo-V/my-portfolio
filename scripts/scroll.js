c53338a60c1b034104dcd920ac822b47ab7086cb
Alveira <alveiramurilo@hotmail.com>
2020-08-09 20:24:49 -0300

    feat: added projects

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,4 +1,4 @@
-function welcomeScroll(){
+function presentationScroll(){
         targetOffset = $("#presentation").offset().top
         $('html, body').animate({
           scrollTop: targetOffset - 80
@@ -7,6 +7,14 @@ function welcomeScroll(){
 
 function skillsScroll(){
   targetOffset = $("#hard-skills").offset().top
+  $('html, body').animate({
+    scrollTop: targetOffset - 80
+  }, 700);
+}
+
+
+function projectsScroll(){
+  targetOffset = $("#projects").offset().top
   $('html, body').animate({
     scrollTop: targetOffset - 80
   }, 700);
