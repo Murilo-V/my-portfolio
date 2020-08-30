91b6990cf364201f3f67a752b9289d89a8c18b7c
Murilo-V <alveiramurilo@hotmail.com>
2020-08-30 19:56:15 -0300

    feat: topbutton

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,7 @@
 const bgHeader = document.getElementById('navbar');
 const muriloHeader = document.getElementById('murilo');
 const iconHeader = document.getElementById('menu-icon');
+const topButton = document.getElementById('topButton')
 
 bgHeader.style.transition = "150ms";
 bgHeader.style.transitionProperty = "background-color";
@@ -11,26 +12,32 @@ window.addEventListener("load",() => {
             bgHeader.style.backgroundColor = "";
             muriloHeader.style.color = "#3F8D1A";
             iconHeader.style.color = "#3F8D1A";
+            topButton.style.opacity = "0%";
         }
         if ($(window).scrollTop() > $("#presentation").offset().top - 120){
             bgHeader.style.backgroundColor = "#D5DBD5";
             muriloHeader.style.color = "#323232";
             iconHeader.style.color = "#323232";
+            topButton.style.opacity = "100%";
+            topButton.style.color = "#323232";
         }
         if ($(window).scrollTop() > $("#skills").offset().top){
             bgHeader.style.backgroundColor = "#3F8D1A";
             muriloHeader.style.color = "#D5DBD5";
             iconHeader.style.color = "#D5DBD5";
+            topButton.style.color = "#D5DBD5";
         }
         if ($(window).scrollTop() > $("#projects").offset().top){
             bgHeader.style.backgroundColor = "#323232";
             muriloHeader.style.color = "#3F8D1A";
             iconHeader.style.color = "#3F8D1A";
+            topButton.style.color = "#3F8D1A"
         }
         if ($(window).scrollTop() > $("#contact").offset().top){
             bgHeader.style.backgroundColor = "#8A8A8A";
             muriloHeader.style.color = "#323232";
             iconHeader.style.color = "#323232";
+            topButton.style.color = "#323232"
         }
     });   
 });
