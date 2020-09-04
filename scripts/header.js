79950320cc02e7a799397aa03d8d1f60afae00bf
Murilo-V <alveiramurilo@hotmail.com>
2020-09-04 19:25:26 -0300

    feat: topbutton function

--- scripts/header.js
+++ scripts/header.js
@@ -18,26 +18,23 @@ window.addEventListener("load",() => {
             bgHeader.style.backgroundColor = "#D5DBD5";
             muriloHeader.style.color = "#323232";
             iconHeader.style.color = "#323232";
-            topButton.style.opacity = "100%";
-            topButton.style.color = "#323232";
+            topButton.style.opacity = "0%";
         }
         if ($(window).scrollTop() > $("#skills").offset().top){
             bgHeader.style.backgroundColor = "#3F8D1A";
             muriloHeader.style.color = "#D5DBD5";
             iconHeader.style.color = "#D5DBD5";
-            topButton.style.color = "#D5DBD5";
+            topButton.style.opacity = "100%";
         }
         if ($(window).scrollTop() > $("#projects").offset().top){
             bgHeader.style.backgroundColor = "#323232";
             muriloHeader.style.color = "#3F8D1A";
             iconHeader.style.color = "#3F8D1A";
-            topButton.style.color = "#3F8D1A"
         }
         if ($(window).scrollTop() > $("#contact").offset().top){
             bgHeader.style.backgroundColor = "#8A8A8A";
             muriloHeader.style.color = "#323232";
             iconHeader.style.color = "#323232";
-            topButton.style.color = "#323232"
         }
     });   
 });
---
+++ scripts/top-button.js
@@ -0,0 +1,12 @@
+function scrollTopButton() {
+    const bgHeader = document.getElementById('navbar');
+    const muriloHeader = document.getElementById('murilo');
+    const iconHeader = document.getElementById('menu-icon');
+    const topButton = document.getElementById('topButton');
+
+    bgHeader.style.backgroundColor = "";
+    muriloHeader.style.color = "#3F8D1A";
+    iconHeader.style.color = "#3F8D1A";
+    topButton.style.opacity = "0%";
+    window.scrollTo(0,0);
+}
